4a803355ab8fe0be251d671cdfb8fb62ddb56a42
Juan Marcon <juanmarcon@MacBook-Pro-de-Juan.local>
2024-02-12 14:23:50 -0500

    fix(libs-prisma-schema): add nullable updated-at column

---
+++ libs/prisma-schema/prisma/migrations/20240212192151_fix_nullable_updated_at/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `Action` MODIFY `updatedAt` DATETIME(3) NULL;
+
+-- AlterTable
+ALTER TABLE `Role` MODIFY `updatedAt` DATETIME(3) NULL;
+
+-- AlterTable
+ALTER TABLE `User` MODIFY `updatedAt` DATETIME(3) NULL;
