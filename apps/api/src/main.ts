dd7901d3b30676049dd8811135fab3893fae08b5
juanse1080 <juanmarcon1080@gmail.com>
2024-02-13 19:40:31 -0500

    chore(api): add link to download json docs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,14 +7,17 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  const docPrefix = 'docs';
 
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   const port = process.env.PORT || 3000;
 
@@ -22,14 +25,17 @@ async function bootstrap() {
     .setTitle('Nest interview API')
     .setDescription('API builded with Nest JS')
     .setVersion('1.0')
+    .setExternalDoc('Download JSON Specifications', '/docs-json')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
+  SwaggerModule.setup(docPrefix, app, document);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+  Logger.log(
+    `🚀 Application docs is running on: http://localhost:${port}/${docPrefix}`
   );
 }
 
