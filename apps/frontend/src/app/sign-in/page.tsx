19aaee8b3de551f4e5d58bc2abc1125024323a9c
juanse1080 <juanmarcon1080@gmail.com>
2024-02-15 13:28:08 -0500

    fix(ui): migrate to builder=none lib

--- apps/frontend/src/app/sign-in/page.tsx
+++ apps/frontend/src/app/sign-in/page.tsx
@@ -1,7 +1,23 @@
+import { Button, Input } from '@nest-interview/ui';
+
 export default async function SignIn() {
   return (
-    <div className="grid gap-4">
+    <form className="flex flex-col gap-4">
       <h1>Sign In</h1>
-    </div>
+      <Input
+        label="Email"
+        name="email"
+        placeholder="example@domain.com"
+        required
+      />
+      <Input
+        label="Password"
+        name="password"
+        type="password"
+        placeholder="example@domain.com"
+        required
+      />
+      <Button>Submit</Button>
+    </form>
   );
 }
--- libs/ui/src/lib/components/button/button.tsx
+++ libs/ui/src/lib/components/button/button.tsx
@@ -1,18 +1,24 @@
 import { ButtonHTMLAttributes } from 'react';
 import { merge } from '../../utils';
 
-export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  fullWidth?: boolean;
+};
 
 export function Button({
   className,
   disabled,
+  fullWidth,
   ...props
 }: Readonly<ButtonProps>) {
   return (
     <button
       className={merge(
-        'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg',
-        { 'cursor-not-allowed opacity-50 hover:bg-blue-500': disabled },
+        'w-min bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg',
+        {
+          'cursor-not-allowed opacity-50 hover:bg-blue-500': disabled,
+          'w-full': fullWidth,
+        },
         className
       )}
       disabled={disabled}
